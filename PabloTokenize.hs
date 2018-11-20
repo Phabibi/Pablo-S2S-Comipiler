6e9ac4512d9884744cfb154d2596027e468844db
Parsa Habibi <phabibi@sfu.ca>
2018-11-19 20:59:03 -0800

    the toknizer now works

--- PabloTokenize.hs
+++ PabloTokenize.hs
@@ -4,7 +4,7 @@ import Data.Char
 
 data Token = OR | XOR | AND | NEG | Lparen | Rparen | LangleB
  | RangleB | Lbrak | Rbrak | I Int | COLCOL | Lbrace | Rbrace
- | Kernal [Char] | Arrow | Com | BadToken Char | Special [Char] | If | While | Col deriving Show
+ | Kernel | Arrow | Com | BadToken Char | Special [Char] | IF | While | Col deriving Show
 
 tokenize [] = []
 tokenize (' ':more) = tokenize more  -- ignore white space
@@ -23,30 +23,37 @@ tokenize ('<':more) = LangleB: (tokenize more)
 tokenize ('>':more) = RangleB : (tokenize more)
 tokenize (',':more) = Com: (tokenize more)
 tokenize (':':[]) = [Col] --useless but what only gets hit when theres only 1 thing in the array and its just a col
-tokenize ('i':(c:more))
-   | isDigit c = let (I n: tokens) = getNumToken (digitToInt c) more
-                   in ((I n): tokens)
-   | otherwise = (Special "i"):(tokenize (c:more))
+tokenize ('w':'h':'i':'l':'e':more) = While:(tokenize more)
+tokenize ('i':'f':more) = IF:(tokenize more)
+tokenize ('k':'e':'r':'n':'e':'l':more) = Kernel:(tokenize more)
+
+--TODO:  ask about this case
+
+-- tokenize ('i':(c:more))
+--    | isDigit c = let (I n: tokens) = getNumToken (digitToInt c) more
+--                    in ((I n): tokens)
+--    | otherwise = (Special "i"):(tokenize (c:more))
 
 tokenize (':':c:more)
    |  c == ':' = getSpecial ("::",more)
    | otherwise = Col: (tokenize (c:more))
 tokenize t@(c:more)
    | isDigit c = getNumToken (digitToInt c) more
+   | isAlpha c = getAlphaNum [c] (span isAlphaNum more)
    | otherwise = getSpecial (span (\c -> elem c "+-*/<>=&|!@#$%?:") t)
 
 -- the specials , not really sure what todo
+getSpecial ("", (c : more)) = BadToken c : (tokenize more)
 getSpecial ("::",more) = COLCOL: (tokenize more)
 getSpecial ("->",more) = Arrow: (tokenize more)
-
--- TODO: stuck on these ask
- 
--- getSpecial ("while",more) = While: (tokenize more)
--- getSpecial ("if",more) = If: (tokenize more) not too sure how to handle these cases yet
--- getSpecial ("kernal",more)
-
+getSpecial (specials, more) = (Special specials) : (tokenize more)
 ---
 getNumToken accum [] = [I accum]
 getNumToken accum (c:more)
    | isDigit c  = getNumToken (accum * 10 + (digitToInt c)) more
    | otherwise  = I accum : (tokenize (c:more))
+   
+getAlphaNum pfx (alphanum, s@('i':c:more))
+   | isAlphaNum c   = getAlphaNum (pfx ++ alphanum ++ ['i',c]) (span isAlphaNum more)
+   | otherwise      = Special (pfx ++ alphanum) : (tokenize s)
+getAlphaNum pfx (alphanum, more) = Special (pfx ++ alphanum) : (tokenize more)
